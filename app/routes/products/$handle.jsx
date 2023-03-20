ddf1ed08caf6d717209c39e7e4027326f915328d
AlejandroChacin <ajchacin1307@gmail.com>
2023-03-20 17:11:51 -0400

    Cart components.

--- app/routes/products/$handle.jsx
+++ app/routes/products/$handle.jsx
@@ -3,6 +3,7 @@ import { json } from 'react-router';
 import { MediaFile } from '@shopify/hydrogen-react';
 import ProductOptions from '~/components/ProductOptions';
 import { MediaFile, Money, ShopPayButton } from '@shopify/hydrogen-react';
+import { useMatches, useFetcher } from '@remix-run/react';
 
 export const loader = async ({ params, context, request }) => {
   const { handle } = params;
@@ -70,7 +71,7 @@ export default function ProductHandle() {
                 variantIds={[selectedVariant?.id]}
                 width={'400px'}
               />
-              {/* TODO product form */}
+              <ProductForm variantId={selectedVariant?.id} />
             </div>
           )}
           <div
@@ -137,6 +138,29 @@ function ProductGallery({ media }) {
         );
       })}
     </div>
+  );
+}
+
+function ProductForm({ variantId }) {
+  const [root] = useMatches();
+  const selectedLocale = root?.data?.selectedLocale;
+  const fetcher = useFetcher();
+
+  const lines = [{ merchandiseId: variantId, quantity: 1 }];
+
+  return (
+    <fetcher.Form action="/cart" method="post">
+      <input type="hidden" name="cartAction" value={'ADD_TO_CART'} />
+      <input
+        type="hidden"
+        name="countryCode"
+        value={selectedLocale?.country ?? 'US'}
+      />
+      <input type="hidden" name="lines" value={JSON.stringify(lines)} />
+      <button className="bg-black text-white px-6 py-3 w-full rounded-md text-center font-medium max-w-[400px]">
+        Add to Bag
+      </button>
+    </fetcher.Form>
   );
 }
 
