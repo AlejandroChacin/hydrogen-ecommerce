13cac41f8faed7ae3cdc184b731aae1a66cf1991
AlejandroChacin <ajchacin1307@gmail.com>
2023-03-20 17:04:30 -0400

    Display title, description and product images.

--- app/root.jsx
+++ app/root.jsx
@@ -10,6 +10,7 @@ import tailwind from './styles/tailwind-build.css';
 import favicon from '../public/favicon.svg';
 import { Layout } from './components/Layout';
 import { Seo } from '@shopify/hydrogen';
+import { ShopifyProvider } from '@shopify/hydrogen-react';
 
 export const links = () => {
   return [
@@ -38,26 +39,35 @@ export async function loader({ context }) {
 
 export default function App() {
   const data = useLoaderData();
-
-  const { name } = data.layout.shop;
+  const { name, description } = data.layout.shop;
 
   return (
-    <html lang="en">
-      <head>
-        <Seo />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Layout title={name}>
-          <Outlet />
-        </Layout>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <ShopifyProvider {...shopifyConfig}>
+      <html lang="en">
+        <head>
+          <Seo />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <Layout title={name}>
+            <Outlet />
+          </Layout>
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
+    </ShopifyProvider>
   );
 }
+
+const shopifyConfig = {
+  storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+  storeDomain: 'https://hydrogen-preview.myshopify.com',
+  storefrontApiVersion: '2023-01',
+  countryIsoCode: 'US',
+  languageIsoCode: 'en',
+};
 
 const LAYOUT_QUERY = `#graphql
   query layout {
